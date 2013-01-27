271d97c1a721bec4a1bc4fccf9011865ee0bb75b
George Lewis <ghl227@nyu.edu>
2013-01-26 20:06:58 -0800

    Importing Flask website skeleton

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
+    
+

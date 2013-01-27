c78f641555a1bd7cd0a3fcd6c8bbd3bd912fded9
George Lewis <ghl227@nyu.edu>
2013-01-27 14:51:20 -0800

    Only one marker is created on index map which return long/lat info

--- static/mapping.js
+++ static/mapping.js
@@ -12,6 +12,19 @@ GEvent.addListener(map, "click", function(marker,point) {
 });
 */
 
+var marker = null;
+
+function placeMarker(map, location) {
+
+    // Create if necessary
+    if( !marker ){
+	marker = new google.maps.Marker({position: location, map: map});
+    }
+
+    // Set its position
+    marker.setPosition(location);
+}
+
 $(document).ready(function() {
     
     console.log("Loading Map");
@@ -21,17 +34,25 @@ $(document).ready(function() {
 	zoom: 12,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     var map = new google.maps.Map(document.getElementById("map_canvas"),
 				  mapOptions);
+
+    google.maps.event.addListener(map, 'click', function(event) {
+	placeMarker(map, event.latLng);
+	console.log( marker['position']['Ya'] );
+	console.log( marker['position']['Za'] );
+    });
     
     console.log("Loaded Map");
 
+    /*
     google.maps.event.addListener(map, 'click', function(event) {
         marker = new google.maps.Marker({position: event.latLng, map: map});
 	console.log(marker);
 	console.log( marker['position']['Ya'] );
 	console.log( marker['position']['Za'] );
     });
-
+    */
 
 });

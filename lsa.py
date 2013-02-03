0ed8eeccdd0442822fc9f9160aeaa76fce1d2060
George Lewis <ghl227@nyu.edu>
2013-02-02 17:59:16 -0800

    Refactored lsa and added cosines to weight function

--- lsa.py
+++ lsa.py
@@ -38,6 +38,7 @@ def get_stopwords():
 
 class LSA(object):
 
+
     def __init__(self, stopwords=None, ignorechars=None):
         self.stopwords = stopwords
         self.ignorechars = ignorechars
@@ -47,6 +48,7 @@ class LSA(object):
         # doc_dict[doc_name] = column
         self.doc_dict = {}
         self.dimensions = 20
+
 
     def parse_document(self, doc_name, doc):
         words = doc.split();
@@ -66,11 +68,14 @@ class LSA(object):
         self.doc_dict[doc_name] = self.dcount
         self.dcount += 1
 
+
     def build_matrix(self):
         """
         Create the internal numpy matrix:
         A[word_idx, document_idx]
         """
+
+        print "Building Matrix"
         self.keys = [k for k in self.wdict.keys() if len(self.wdict[k]) > 1]
         self.keys.sort()
         self.A = zeros([len(self.keys), self.dcount])
@@ -83,7 +88,10 @@ class LSA(object):
         print "Documents: %s" % len(self.doc_dict)
         print "Shape: ", self.A.shape 
 
+
     def tf_idf(self):
+
+        print "TF_IDF"
         WordsPerDoc = numpy.sum(self.A, axis=0)
         DocsPerWord = numpy.sum(asarray(self.A > 0, 'i'), axis=1)
         rows, cols = self.A.shape
@@ -91,7 +99,11 @@ class LSA(object):
             for j in range(cols):
                 self.A[i,j] = (self.A[i,j] / WordsPerDoc[j]) * log(float(cols) / DocsPerWord[i])
 
+
     def run_svd(self):
+        """
+        Run the Singular Value Decomposition
+        """
         print "Running SVD"
         self.U, self.S, self.Vt = svd(self.A, full_matrices=False)
 
@@ -122,6 +134,7 @@ class LSA(object):
         print "Reduced column for %s" % 'Bantam'
         print self.get_svd_document_vector('Bantam').shape
 
+
     def check_consistency(self):
         
         middle = numpy.zeros((len(self.S), len(self.S)))
@@ -133,23 +146,9 @@ class LSA(object):
         print self.A.shape
         print self.A[0:5, 0:5]
         print "Matrix is consistent: ", numpy.allclose(self.A, prod, 0.0001)
+
         
-    def top_svd(self, num=None):
-        
-        # Get the index for sorting:
-        print self.S
-        sorted_index = [i[0] for i in sorted(enumerate(self.S), key=lambda x: x[1])]
-        print sorted_index
-        return
-
-        print "S: ", self.S.shape, self.S
-        print "S Sorted: ", numpy.sort(self.S)
-        return numpy.sort(self.S)[::-1]
-
-    def printA(self):
-        print self.A
-
-    def get_document_vector(self, name):
+    def _get_document_vector(self, name):
         """
         Print the row corresponding to
         the given restaurant
@@ -158,13 +157,18 @@ class LSA(object):
         column = self.doc_dict[name]
         return self.A[:,column]
 
+
     def get_svd_document_vector(self, name):
         """
         Print the row corresponding to
         the given restaurant
         """
         # Get the column for the document
-        column = self.doc_dict[name]
+        try:
+            column = self.doc_dict[name]
+        except KeyError:
+            print "Venue %s not found in svd corpus" % name
+            raise
         return self.Vt[:,column]
 
 
@@ -176,7 +180,10 @@ class LSA(object):
         
         doc_vec = self.get_svd_document_vector(doc)
         doc_vec = doc_vec[:len(user_vec)]
-        return numpy.dot(vecA, vecB)
+        print "Getting Cosine: ",
+        for a, b in zip(user_vec, doc_vec):
+            print "(%s, %s)" % (a, b),
+        return numpy.dot(user_vec, doc_vec)
 
     
     def update(self, user_vec, doc_vec, accepted=True, beta=0.5):
@@ -242,18 +249,16 @@ class LSA(object):
         Save any items that we need to a file
         """
 
+        print "Saving: ", self
+
         # Save the matrices
-        print "Saving 'A' %s %s kb " % (self.A.shape, self.A.nbytes/1000)
         numpy.save("lsa_A.npy", self.A)
-
-        print "Saving 'U' %s %s kb" % (self.U.shape, self.U.nbytes/1000)
         numpy.save("lsa_U.npy", self.U)
-
-        print "Saving 'S' %s %s kb" % (self.S.shape, self.S.nbytes/1000)
         numpy.save("lsa_S.npy", self.S)
-
-        print "Saving 'Vt' %s %s kb" % (self.Vt.shape, self.Vt.nbytes/1000)
         numpy.save("lsa_Vt.npy", self.Vt)
+
+        with open('lsa_info.txt', 'wb') as fp:
+            fp.write(str(self))
 
         # Save the document dictionary
         with open('doc_dict.json', 'wb') as fp:
@@ -282,6 +287,18 @@ class LSA(object):
             self.keys = json.load(fp)
 
 
+    def __repr__(self):
+        """
+        Implement the string representation
+        """
+        lsa_str = "LSA Object \n"
+        lsa_str += "A %s %s kb \n" % (self.A.shape, self.A.nbytes/1000)
+        lsa_str += "U %s %s kb \n" % (self.U.shape, self.U.nbytes/1000)
+        lsa_str += "S %s %s kb \n" % (self.S.shape, self.S.nbytes/1000)
+        lsa_str += "Vt %s %s kb \n" % (self.Vt.shape, self.Vt.nbytes/1000)
+        return lsa_str
+
+
 def main():
 
     parser = argparse.ArgumentParser(description='Scrape data from NYMag.')
@@ -291,7 +308,7 @@ def main():
                         help='Test the lsa')
     parser.add_argument('--limit', '-l', dest='limit', type=int, default=None, 
                         help='Maximum number of venues to use in sva matrix (default is all)')
-    parser.add_argument('--size', '-s', dest='size', type=int, default=40, 
+    parser.add_argument('--size', '-s', dest='size', type=int, default=None, 
                         help='Number of Support Vector dimensions to use')
     args = parser.parse_args()
 
@@ -322,13 +339,21 @@ def main():
         lsa.tf_idf()
         lsa.run_svd()
         lsa.check_consistency()
-        lsa.reduce(args.size)
-        lsa.printA()
+        if args.size != None:
+            lsa.reduce(args.size)
         lsa.save()
 
     if args.test:
 
         lsa.load()
+
+        if args.size != None:
+            lsa.reduce(args.size)
+
+        print "Singular Values:"
+        for value in lsa.S:
+            print value
+
         test_bars = ["Bantam", "1 Oak", "Amity Hall", "Ainsworth Park", "B Bar & Grill", 
                      "Ajna Bar", "Amsterdam Ale House", "2nd Floor on Clinton"]
         #test_bars = [u"Art Bar", u"The Back Room", u"The Back Fence"]

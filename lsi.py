225acc603ad89d6ac9f1d66bc589e80f9947cf2b
George Lewis <ghl227@nyu.edu>
2013-02-05 10:15:17 -0800

    Added deserialization of corpus, dictionary, and lsi object

--- lsi.py
+++ lsi.py
@@ -1,13 +1,16 @@
 
-from nymag_scrape import connect_to_database
-from gensim.models.lsimodel import LsiModel
-from gensim import corpora, models, similarities
 
 import logging
 
 import nltk
 from nltk.corpus import wordnet
 import unicodedata
+
+from nymag_scrape import connect_to_database
+
+from gensim.models.lsimodel import LsiModel
+from gensim import corpora, models, similarities
+
 
 def get_stopwords():
     """
@@ -37,41 +40,19 @@ def tokenize_document(doc, stopwords, ignorechars):
         w = w.lower().translate(None, ignorechars)
 
         # Skip invalid words
-        if w == '': 
-            continue
-        if w in stopwords:
-            continue
-        if not wordnet.synsets(w):
-            continue
+        if w == '': continue
+        if w in stopwords: continue
+        if not wordnet.synsets(w): continue
         has_number = False
         for num in [str(num) for num in range(10)]:
             if num in w:
                 has_number = True
                 break
-        if has_number:
-            continue
+        if has_number: continue
 
         tokens.append(w)
         
     return tokens
-
-
-def create_texts(doc_iterator):
-
-    texts = []
-
-    for doc in doc_iterator:
-        tokens = tokenize_document(doc)
-        texts.append(tokens)
-
-    # Get all tokens in our corpus
-    # and remove words that appear only once
-    all_tokens = sum(texts, [])
-    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-    texts = [ [word for word in text if word not in tokens_once]
-             for text in texts]
-
-    return texts
 
 
 def remove_words_appearing_once(texts):
@@ -84,6 +65,34 @@ def remove_words_appearing_once(texts):
     texts = [ [word for word in text if word not in tokens_once]
              for text in texts]
     return texts
+
+
+def create_string_from_database(db_entry):
+    """
+    Take a location database entry and
+    convert it into a single string, 
+    including reviews and tips
+    """
+
+    nymag = db_entry['nymag']
+    name = nymag['name']
+    text = nymag['review']
+    fsq_tips = db_entry['foursquare']['tips']
+    text.join([tip['text'] for tip in fsq_tips])
+
+    return text
+
+
+def load_lsi():
+    """
+    Load the saved-on-disk lsi
+    object and return it.
+    """
+    lsi = models.LsiModel.load('model.lsi')
+    corpus = corpora.MmCorpus('lsi.corpus.mm')
+    dictionary = corpora.Dictionary.load('lsi.dict')
+
+    return (lsi, corpus, dictionary)
     
 
 def main():
@@ -93,7 +102,7 @@ def main():
     # Add documents
     db, connection = connect_to_database(table_name="barkov_chain")
     bars = db['bars']
-    num_bars = 50
+    num_bars = 200
     locations = bars.find({ 'nymag.review' : {'$ne':None}, 
                             'foursquare.tips' : {'$exists':True}, 
                             'foursquare.tips' : {'$ne':None} 
@@ -105,13 +114,16 @@ def main():
     texts = []
     
     for location in locations:
-        nymag = location['nymag']
-        name = nymag['name']
-        description = nymag['review']
-        fsq_tips = location['foursquare']['tips']
-        description.join([tip['text'] for tip in fsq_tips])
-        tokens = tokenize_document(description, stopwords, ignorechars)
+        text = create_string_from_database(location)
+        tokens = tokenize_document(text, stopwords, ignorechars)
         texts.append(tokens)
+        #nymag = location['nymag']
+        #name = nymag['name']
+        #description = nymag['review']
+        #fsq_tips = location['foursquare']['tips']
+        #description.join([tip['text'] for tip in fsq_tips])
+        #tokens = tokenize_document(description, stopwords, ignorechars)
+        #texts.append(tokens)
 
     # Do some cleaning
     texts = remove_words_appearing_once(texts)
@@ -128,12 +140,16 @@ def main():
     corpus_tfidf = tfidf[corpus]
 
     # Create the model
-    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=5) 
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=10) 
     corpus_lsi = lsi[corpus_tfidf]
+    print '\n'
+    lsi.print_topics(10)
+    return
     #lsi.print_topics(2)
 
     lsi.save('model.lsi')
 
+    
     index = similarities.MatrixSimilarity(lsi[corpus])
 
     test_vector = lsi[corpus[0]]
